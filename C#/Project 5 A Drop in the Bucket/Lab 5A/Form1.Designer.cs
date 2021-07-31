c42fc992d775908cf0e8345086d2496433eba14f
AttaULSaboor <86814572+AttaULSaboor@users.noreply.github.com>
2021-07-30 23:25:17 -0400

    C # Project

---
+++ C#/Project 5 A Drop in the Bucket/Lab 5A/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Lab_5A
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.trackBar = new System.Windows.Forms.TrackBar();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.changeColorBTN = new System.Windows.Forms.Button();
+			this.closeBTN = new System.Windows.Forms.Button();
+			this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// trackBar
+			// 
+			this.trackBar.Location = new System.Drawing.Point(55, 93);
+			this.trackBar.Name = "trackBar";
+			this.trackBar.Size = new System.Drawing.Size(104, 45);
+			this.trackBar.TabIndex = 0;
+			this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Black;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(55, 164);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(90, 68);
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// changeColorBTN
+			// 
+			this.changeColorBTN.BackColor = System.Drawing.Color.White;
+			this.changeColorBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.changeColorBTN.Location = new System.Drawing.Point(55, 16);
+			this.changeColorBTN.Name = "changeColorBTN";
+			this.changeColorBTN.Size = new System.Drawing.Size(90, 25);
+			this.changeColorBTN.TabIndex = 4;
+			this.changeColorBTN.Text = "Colour";
+			this.changeColorBTN.UseVisualStyleBackColor = false;
+			this.changeColorBTN.Click += new System.EventHandler(this.changeColorBtn_Click);
+			// 
+			// closeBTN
+			// 
+			this.closeBTN.BackColor = System.Drawing.Color.White;
+			this.closeBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.closeBTN.Location = new System.Drawing.Point(185, 17);
+			this.closeBTN.Name = "closeBTN";
+			this.closeBTN.Size = new System.Drawing.Size(90, 25);
+			this.closeBTN.TabIndex = 5;
+			this.closeBTN.Text = "Close";
+			this.closeBTN.UseVisualStyleBackColor = false;
+			this.closeBTN.Click += new System.EventHandler(this.exitBTN_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.ClientSize = new System.Drawing.Size(352, 487);
+			this.Controls.Add(this.closeBTN);
+			this.Controls.Add(this.changeColorBTN);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.trackBar);
+			this.Name = "Form1";
+			this.Text = "A Drop in the Bucket";
+			((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TrackBar trackBar;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button changeColorBTN;
+		private System.Windows.Forms.Button closeBTN;
+		private System.Windows.Forms.ColorDialog colorDialog1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ C#/Project 5 A Drop in the Bucket/Lab 5A/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+/*
+ * Prepared by : Atta UL Saboor
+ * Date : December 11, 2020
+ * 
+ * Program Purpose: 
+ * The purpose of this program is to display a graphics object to graphically display a
+ * liquid filling a bucket and use a timmer control to display the bucket contents as it
+ * fills furthermore use a trackbar control to adjust the flow rate of the liquid and 
+ * use the color dialog control.
+ * 
+ * Statement of Authorship:
+ * I, Atta UL Saboor, 000395780 certify that this material is my original work. No other 
+ * person's work has been used without due acknowledgement.
+ * 
+ */
+
+namespace Lab_5A
+{
+	public partial class Form1 : Form
+	{
+		public Color selectedColor;         //store selected color
+		public int speed;                   //store speed of falling liquid
+		public int fillPosition = 455;      //store position liquied in box
+		public int dropPosition = 260;      //store position of droping liquid
+		public bool newBox = false;         //new box 
+		public const int fillLimit = 350;	//store max fill limit for box
+
+		/// <summary>
+		/// constructor
+		/// </summary>
+		public Form1()
+		{
+			InitializeComponent();
+			this.Paint += new PaintEventHandler(DrawBox);
+			selectedColor = Color.SkyBlue;	//default selected color
+		}
+
+		/// <summary>
+		/// Draw box on form load
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void DrawBox(object sender, PaintEventArgs e)
+		{
+			Graphics graphics = e.Graphics;
+			Pen pen = new Pen(Color.White);
+
+			//draw Box
+			graphics.DrawLine(pen, 100, 455, 100, 340);
+			graphics.DrawLine(pen, 320, 455, 100, 455);
+			graphics.DrawLine(pen, 320, 340, 320, 455);
+		}
+
+		/// <summary>
+		/// Change Color button is clicked  set tht user selected color 
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void changeColorBtn_Click(object sender, EventArgs e)
+		{
+			//selected color
+			if (colorDialog1.ShowDialog() == DialogResult.OK)
+				selectedColor = colorDialog1.Color;
+		}
+
+		/// <summary>
+		/// Exit button is clicked close the application
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void exitBTN_Click(object sender, EventArgs e)
+		{
+			Environment.Exit(1);		//close application
+		}
+
+		/// <summary>
+		/// Track bar value changes set the draw speed and reset application when box is full
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void trackBar_Scroll(object sender, EventArgs e)
+		{
+			if(trackBar.Value > 0)
+			{
+				if (!newBox)
+				{
+					//set speed for fill the box
+					switch (trackBar.Value)
+					{
+						case 1: speed = 250; break;
+						case 2: speed = 200; break;
+						case 3: speed = 150; break;
+						case 4: speed = 125; break;
+						case 5: speed = 100; break;
+						case 6: speed = 50; break;
+						default: speed = 20; break;
+					}
+
+					//enable timer
+					timer1.Interval = speed;
+					timer1.Enabled = true;
+				}
+				else ResetApplication();
+			}
+			else //hide droping water from tab
+			{
+				speed = 0;
+				timer1.Enabled = false;     //stop timer
+
+				//dropping tab water with black water hide it
+				Graphics graphics = base.CreateGraphics();
+				graphics.FillRectangle(new SolidBrush(Color.Black), 125, 195, 14, dropPosition);
+			}
+		}
+
+		/// <summary>
+		/// Application reset when box is full and set value to default
+		/// </summary>
+		private void ResetApplication()
+		{
+			newBox = false;             // reset new box 
+
+			//water position reset
+			fillPosition = 455;     // water in box
+			dropPosition = 260;     // falling water from tap
+
+			Graphics graphics = base.CreateGraphics();
+			Pen pen = new Pen(Color.White);
+
+			graphics.Clear(Color.Black);    // clear previous graphics
+
+			//draw box
+			graphics.DrawLine(pen, 100, 455, 100, 340);
+			graphics.DrawLine(pen, 320, 455, 100, 455);
+			graphics.DrawLine(pen, 320, 340, 320, 455);
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (fillPosition != fillLimit)
+			{
+				fillPosition--;				// water update in box position
+				dropPosition--;             // water falling update from tab position
+
+				Graphics graphics = base.CreateGraphics();
+				Pen pen = new Pen(selectedColor);
+
+				// water in box
+				graphics.DrawLine(pen, 319, fillPosition, 101, fillPosition);
+
+				// falling water from tab
+				graphics.FillRectangle(new SolidBrush(selectedColor), 125, 195, 14, dropPosition);
+			}
+			else        //box is full
+			{
+				timer1.Enabled = false;
+				newBox = true;
+				trackBar.Value = 0;
+
+				// hide falling water when box full
+				Graphics graphics = base.CreateGraphics();
+				graphics.FillRectangle(new SolidBrush(Color.Black), 125, 195, 14, dropPosition);
+			}
+		}
+	}
+}
