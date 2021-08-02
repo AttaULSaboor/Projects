8ef552d3773726993f3f728f7bfbfbcc8519becd
Atta UL Saboor <86814572+AttaULSaboor@users.noreply.github.com>
2021-08-02 08:55:19 -0400

    Update connect.php

--- PHP, JavaScript & MySQL/Project 1 Hunt the Wampus/connect.php
+++ PHP, JavaScript & MySQL/Project 1 Hunt the Wampus/connect.php
@@ -2,8 +2,8 @@
     try {
         $dbh = new PDO(
             "mysql:host=localhost;dbname=000395780",
-            "000395780",
-            "19861007"
+            "root",
+            ""
         );
     } catch (Exception $e){
         die("ERROR: Couldn't connect. {$e->getMessage()}");
