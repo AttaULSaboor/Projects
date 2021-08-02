016dd5c629a8054a503880b4e454eaa961e07e98
Atta UL Saboor <86814572+AttaULSaboor@users.noreply.github.com>
2021-08-02 17:17:05 -0400

    ASP.NET CORE & MVC Projects

---
+++ ASP.NET CORE & MVC/Project 3 NorthWind Traders Site/assignment4/Northwind/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Models
+{
+	public class Product
+	{   [Key]
+        public int ProductId { get; set; }
+        [Display(Name = "Name")]
+        public string ProductName { get; set; }
+        [Display(Name = "Supplier Id")]
+        public int? SupplierId { get; set; }
+        [Display(Name = "Category")]
+        public int? CategoryId { get; set; }
+        [Display(Name = "Quantity Per Unit")]
+        public string QuantityPerUnit { get; set; }
+        [Display(Name = "Unit Price")]
+        public decimal? UnitPrice { get; set; }
+        [Display(Name = "In Stock")]
+        public short? UnitsInStock { get; set; }
+        [Display(Name = "On Order")]
+        public short? UnitsOnOrder { get; set; }
+        [Display(Name = "Recorder Level")]
+        public short? ReorderLevel { get; set; }
+        [Display(Name = "Discontinued")]
+        public bool Discontinued { get; set; }
+
+        public object Category { get; set; }
+        public object Supplier { get; set; }
+        public object[] OrderDetails { get; set; }
+
+        public int SelectedCategory { get; set; }
+    }
+}
