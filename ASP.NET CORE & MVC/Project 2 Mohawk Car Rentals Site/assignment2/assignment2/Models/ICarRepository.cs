eef724a49f2a8c821c9528875f0781360f4bfcdb
Atta UL Saboor <86814572+AttaULSaboor@users.noreply.github.com>
2021-08-02 16:55:32 -0400

    ASP.NET CORE & MVC Projects

---
+++ ASP.NET CORE & MVC/Project 2 Mohawk Car Rentals Site/assignment2/assignment2/Models/ICarRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment2.Models
+{
+	public interface ICarRepository
+	{
+		IEnumerable<Car> GetCars();
+
+		Car GetCar(int id);
+		void CreateCar(Car car);
+		void EditCar(Car car);
+		void DeleteCar(int id);
+	}
+}
---
+++ ASP.NET CORE & MVC/Project 2 Mohawk Car Rentals Site/assignment2/assignment2/Models/PurchaseDateValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment2.Models
+{
+	public class PurchaseDateValidation : ValidationAttribute
+	{
+		public override bool IsValid(object value)
+		{
+			var PurchaseDate = Convert.ToDateTime(value);
+			return PurchaseDate <= DateTime.Now;
+		}
+	}
+}
